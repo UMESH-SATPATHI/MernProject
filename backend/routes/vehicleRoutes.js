805bb663c4812b64e8deb59278304ede1e2e2015
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-04-22 00:51:35 +0530

    refactor: add routing paths for all crud functions

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createVehicle } from "../controllers/vehicleController.js";
+import { createVehicle, getAllVehicles, updateVehicle, deleteVehicle} from "../controllers/vehicleController.js";
 import { verifyToken, allowRoles } from "../controllers/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, allowRoles("owner"), createVehicle);
+router.get("/", getAllVehicles);
+router.get("/:id", getVehicleById);
+router.put("/:id", verifyToken, allowRoles("owner", "admin"), updateVehicle);
+router.delete("/:id", verifyToken, allowRoles("owner", "admin"), deleteVehicle);
 
 export default router;

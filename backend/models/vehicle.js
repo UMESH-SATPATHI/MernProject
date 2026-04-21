673986fe5901cc2fa46309a7612e2cc833661b57
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-04-21 19:09:02 +0530

    feat: Initialize vehicle Schema

---
+++ backend/models/vehicle.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const vehicleSchema = new mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        vehicleName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        vehicleType: {
+            type: String,
+            enum: ['bike', 'car', 'scooter'],
+            required: true
+        },
+        brand: {
+            type: String,
+            required: true
+        },
+        model: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        images: [
+            {
+                url: {
+                    type: String,
+                    required: true,
+                },
+                publicId: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        isAvailable: {
+            type: Boolean,
+            required: true
+        },
+    },
+    { timestamps: true }
+);
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+export default Vehicle;

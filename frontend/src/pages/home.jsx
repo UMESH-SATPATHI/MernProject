d511608415c5d1205017abd94e4e6ea11c7eb877
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-04-21 20:01:53 +0530

    feat: basic testing home UI initialization

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <section>
+            <h1>P2P Vehicle Rental</h1>
+            <p>Rent vehicles from owners near you.</p>
+        </section>
+    );
+}
+
+export default Home;

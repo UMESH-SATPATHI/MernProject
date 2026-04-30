1efdf2f30acee43b29ba3a08b5e6c0fb48e05528
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-05-01 01:56:00 +0530

    gg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import router from "./Router.jsx";
 import { AuthProvider } from "./context/authContext";
 
 function App() {

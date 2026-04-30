cae896bb913d3aee920b116eb744a01fb56b8db5
UMESH-SATPATHI <satpathiumesh.1917@gmail.com>
2026-04-30 23:41:39 +0530

    not much change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import router from "./Router";
 import { AuthProvider } from "./context/authContext";
 
 function App() {
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,20 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import ProtectedRoute from "./components/ProtectedRoutes";
 import Layout from "./layout/Layout";
 import LoginPage from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import Home from "./pages/home";
-import AddVehicle from "./pages/AddVehicle";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Vehicles from "./pages/Vehicles";
+import VehicleDetails from "./pages/VehicleDetails";
+// import AddVehicle from "./pages/AddVehicle";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
-            { index: true, element: <Home /> },
+            { index: true, path: "/home", element: <Home /> },
+            { path: "/vehicles", element: <Vehicles /> },
+            { path: "/vehicles/:id", element: <VehicleDetails /> },
             { path: "/signup", element: <SignUp /> },
             { path: "/login", element: <LoginPage /> },
-            { path: "/AddVehicle", element: <AddVehicle /> },
+            { path: "/profile", element: <ProtectedRoute><Profile /></ProtectedRoute> },
+            // { path: "/AddVehicle", element: <AddVehicle /> },
         ]
     }
 ]);
